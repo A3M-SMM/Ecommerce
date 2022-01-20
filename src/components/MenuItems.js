780975423957c307d56997c9a5bcb8f050f9edec
A3M.SMM <91294477+A3M-SMM@users.noreply.github.com>
2022-01-19 23:43:41 -0300

    Add files via upload

---
+++ src/components/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Inicio',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Servicios',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Planes',
+        url: '#',
+        cName: 'nav-links',
+    },
+    {
+        title: 'Contacto',
+        url: '#',
+        cName: 'nav-links',
+    },
+];
---
+++ src/components/NavBarItems.js
@@ -0,0 +1,4 @@
+NavBarItems{
+    background: #FC0CA4;
+    height:80px;
+}
