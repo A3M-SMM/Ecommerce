570a4f9fcae1b4586e84f7eec069bdd62d953874
A3M.SMM <91294477+A3M-SMM@users.noreply.github.com>
2022-01-21 23:45:01 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <a href="#" class="logo"><span>A3M.</span>SMM</a>
+        <label for="menu-bar" class="fas fa-bars"></label>
+        <nav class="navbar">
+            <a href="#">INICIO</a>
+            <a href="#">SERVICIOS</a>
+            <a href="#">PLANES</a>
+            <a href="#">CONTACTO</a>
+        </nav>
+    </div>
+  );
+}
+
+export default App;
