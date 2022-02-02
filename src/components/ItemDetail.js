c49cbb0f6eeec02f4882ef71a5c6f0d20dc2b503
A3M.SMM <91294477+A3M-SMM@users.noreply.github.com>
2022-02-02 20:40:21 -0300

    Add files via upload

---
+++ src/components/ItemDetail.js
@@ -0,0 +1,87 @@
+const data = [
+    {
+        id:1,
+        nombre: "Paleta de Colores",
+        descripcion: "Basado en 4 Colores", 
+        precio: "50 usd",
+        },
+        
+        {
+        id:2,
+        nombre: "Logotipo", 
+        descripcion: "Elaboracion de Logo, Isologo, Isotipos o Imagotipos",
+        precio: "100 usd",
+        },
+        
+        {
+        id:3,
+        nombre:"Perfil en Redes",
+        descripcion:"Elaboracion perfil de tu red social",
+        precio:"70 usd",
+        },
+        
+        {
+        id:4,
+        nombre: "Asesoria", 
+        descripcion: "Te asesoramos en el manejo de tus redes",
+        precio: "150 usd",
+        },
+        
+        {
+        id:5,
+        nombre: "Branding", 
+        descripcion: "Le damos un cambio de imagen a tu marca",
+        precio: "200 usd",
+        },
+        
+        {
+        id:6,
+        nombre: "Identidad de Marca", 
+        descripcion: "Mejoramos tu identidad o la creamos de cero",
+        precio: "250 usd",
+        },
+        
+        {
+        id:7,
+        nombre: "Historias y Portadas", 
+        descripcion: "Creamos plantillas para tus historias y portadas", 
+        precio: "50 usd",
+        },
+        
+        {
+        id:8,
+        nombre: "Mejora de Logos", 
+        descripcion: "Mejoramos tu logo y/o lo modernizamos", 
+        precio: "120 usd",
+        },
+        
+        {
+        id:9,
+        nombre:"Landing Page", 
+        descripcion:"Creamos una landing para tu megocio", 
+        precio:"600 usd",
+        },
+        
+        {
+        id:10,
+        nombre: "Plantillas", 
+        descripcion: "Creamos set de plantillas para distintas redes, email y presentaciones", 
+        precio:"200 usd",
+        },
+        
+        {
+        id:11,
+        nombre: "UX|UI", 
+        descripcion: "Elaboracion tus interfaces para dispositivos móviles y paginas web",
+        precio:"800 usd",
+        },
+        
+        {
+        id:12,
+        nombre: "Paginas Web", 
+        descripcion: "Elaboracion e implementación de paginas web responsive", 
+        precio: "Cotizar",
+        }
+];
+
+export default data;
