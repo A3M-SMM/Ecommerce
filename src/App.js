7a807dd3c658ab0b37fede6949c02ea98c0175f7
A3M.SMM <91294477+A3M-SMM@users.noreply.github.com>
2022-02-09 16:49:13 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'; 
 import './assets/css/App.css';
+import Search from './components/Search';
+
 
 import Services from './components/Services';
 import data from './components/ItemListContainer';
@@ -31,14 +33,20 @@ function App() {
               <a href="#">CONTACTO</a>
           </nav>
 
+        <div>
+          <input type="text" className="form" placeholder=" Buscar"></input>
+          <input type="submit" className='btn' value="Search"></input>
+        </div>  
+
         <div className="navbar">
           <a href="#">REGISTRO</a>
           <a href="#">LOGIN</a>
-        </div>  
-
+        </div> 
+        
         <div className="components">
           <CartWidget/>
         </div>
+
         </header>
 
       <section className='service'>
@@ -47,85 +55,73 @@ function App() {
         <img src='https://a3m-smm.netlify.app/img/f-icon1.png'></img>
         <div className='Title'><Services nombre="Paleta de Colores" descripcion="Basado en 4 Colores" precio="50 usd"/>
         </div>
-        <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
+        <div className="contador" ><ItemCount/></div>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon2.png'></img>
         <div className='Title'><Services nombre="Logotipo" descripcion="Elaboracion de Logo, Isologo, Isotipos o Imagotipos" precio="100 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon3.png'></img>
         <div className='Title'><Services nombre="Perfil en Redes" descripcion="Elaboracion perfil de tu red social" precio="70 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon1.png'></img>
         <div className='Title'><Services nombre="Asesoria" descripcion="Te asesoramos en el manejo de tus redes" precio="150 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon2.png'></img>
         <div className='Title'><Services nombre="Branding" descripcion="Le damos un cambio de imagen a tu marca" precio="200 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon3.png'></img>
         <div className='Title'><Services nombre="Identidad de Marca" descripcion="Mejoramos tu identidad o la creamos de cero" precio="250 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
     
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon1.png'></img>
         <div className='Title'><Services nombre="Historias y Portadas" descripcion="Creamos plantillas para tus historias y portadas" precio="50 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon2.png'></img>
         <div className='Title'><Services nombre="Mejora de Logos" descripcion="Mejoramos tu logo y/o lo modernizamos" precio="120 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon3.png'></img>
         <div className='Title'><Services nombre="Landing Page" descripcion="Creamos una landing para tu megocio" precio="600 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon1.png'></img>
         <div className='Title'><Services nombre="Plantillas" descripcion="Creamos set de plantillas para distintas redes, email y presentaciones" precio="200 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon2.png'></img>
         <div className='Title'><Services nombre="UX|UI" descripcion="Elaboracion tus interfaces para dispositivos móviles y paginas web" precio="800 usd"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         <div className='box'>
         <img src='https://a3m-smm.netlify.app/img/f-icon3.png'></img>
         <div className='Title'><Services nombre="Paginas Web" descripcion="Elaboracion e implementación de paginas web responsive." precio="Cotizar"/></div>
         <div className="contador"><ItemCount/></div>
-        <a href="#" class="btn">Agregar a Carrito</a>
         </div>
 
         </div>
