f28f4d629cf9d60d1f7f2336953f8f2ba3797643
A3M.SMM <91294477+A3M-SMM@users.noreply.github.com>
2022-02-11 19:36:12 -0300

    Update home.js

--- pages/home.js
+++ pages/home.js
@@ -1,20 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemListContainer from '../components/ItemListContainer';
 import ItemDetail from '../components/ItemDetail';
+import CartContextProvider from "../context/CartContext";
+import { useContext } from "react";
 
 const Home = () => { 
+
+    const test = useContext(CartContext);
     return(
-        <BrowserRouter>
-            <Navbar/>
-                <Routes>
-                <Route exact path="/" element={<div className="slogan">  Si esta en tu mente.. Lo hacemos posible en A3M </div>}/>
-                        <Route exact path='/' element={<ItemListContainer/>} />
-                        <Route exact path= '/servicios/:1' element={<ItemListContainer/>} />
-                        <Route exact path='/item/:idItem' element={<ItemDetail/>}/>
-                        <Route path='*'><div> 404 Not Found! </div> </Route>
-                </Routes>
-            <footer><div>Copyright @A3M.SMM</div></footer> 
-        </BrowserRouter>
+        <CartContextProvider>
+            <BrowserRouter>
+                <Navbar/>
+                    <Routes>
+                    <Route exact path="/" element={<div className="slogan">  Si esta en tu mente.. Lo hacemos posible en A3M </div>}/>
+                            <Route exact path='/' element={<ItemListContainer/>} />
+                            <Route exact path= '/servicios/:1' element={<ItemListContainer/>} />
+                            <Route exact path='/item/:idItem' element={<ItemDetail/>}/>
+                            <Route path='*'><div> 404 Not Found! </div> </Route>
+                    </Routes>
+                <footer><div>Copyright @A3M.SMM</div></footer> 
+            </BrowserRouter>
+        </CartContextProvider>
     );
 }
 
