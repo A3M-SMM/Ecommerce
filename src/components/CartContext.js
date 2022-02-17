2576c41fb5a87367ee48bf49a447f9ec128b9d49
A3M.SMM <91294477+A3M-SMM@users.noreply.github.com>
2022-02-16 22:02:12 -0300

    Update CartContext.js

--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -27,6 +27,8 @@ const CartContextProvider = ({children}) => {
                     nombreItem: item.nombre,
                     precioItem: item.precio,
                     qtyItem: item.qty,
+                    costItem: item.cost,
+                    qtyItem:qty
                 }
             ]);
         }else {
@@ -34,8 +36,28 @@ const CartContextProvider = ({children}) => {
         }    
     }
 
+    const calcPrecioItem =(idItem) => {
+        let index =cartList.map (item => item.idItem).indexOf(idItem);
+        return cartList[index].costItem * cartList[index].qtyItem;
+    }
+
+    const calSubtotal = () => {
+        let totalItems = cartList.map(item=> calcPrecioItem(item.idItem));
+        return totalItems.reduce((valorPrevio, valorActual) => valorPrevio + valorActual);
+    }
+
+    const calculoItems = () => {
+        let cant = cartList.map(item => item.qtyItem);
+        return cant.reduce(((valorPrevio,valorActual) => valorPrevio + valorActual), 0);
+    }
+
+
+    const onAdd = (qty) => {
+        alert ("Tu seleccionaste " + qty + "items");
+    }
+
         return (
-            <CartContext.Provider value={{cartList, removeItems, removeItem, addToCart}}>
+            <CartContext.Provider value={{cartList, removeItems, removeItem, addToCart, calcPrecioItem, calSubtotal, calculoItems}}>
                 {children}
             </CartContext.Provider>
 
